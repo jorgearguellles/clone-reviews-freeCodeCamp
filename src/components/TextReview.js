bcc953cea31a4d8e4ae2ad155b8965c238efe496
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-03-17 11:52:35 -0500

    add/ ReviewContainer & TextReiew components

---
+++ src/components/TextReview.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const TextReview = () => {
+  return (
+    <div className='review-text-container'>
+      <h2 className='review-title'>Title</h2>
+      <p className='review-secondary-text'>text</p>
+      <p className='review-primary-text'>Review</p>
+    </div>
+  );
+};
